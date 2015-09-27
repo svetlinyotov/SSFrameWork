dacf8bd2a778b1f88209077cf496b87c5669feab
SvetlinYotov <svetlin.yotov@gmail.com>
2015-09-27 23:55:09 +0300

    Helper functions added

--- public/index.php
+++ public/index.php
@@ -13,5 +13,11 @@ function env($param) {
 function config($param) {
     return \SSFrame\Config::getInstance()->get($param);
 }
+function asset($path){
+    $uri = "http://" . $_SERVER['HTTP_HOST'] . $_SERVER['SCRIPT_NAME'];
+    $uri = str_replace("/index.php", "", $uri);
+    $uri .= $path;
+    return $uri;
+}
 
 $app->run();
