5f051b1d52b8dd732828dc481acd2b8ed701d927
SvetlinYotov <svetlin.yotov@gmail.com>
2015-09-29 21:52:41 +0300

    Added timeAgo generator

--- ssframe/Common.php
+++ ssframe/Common.php
@@ -156,4 +156,31 @@ class Common
         }
         return bin2hex($buf);
     }
+
+    public static function generateTimeAgo($time)
+    {
+        $periods = array("second", "minute", "hour", "day", "week", "month", "year", "decade");
+        $lengths = array("60","60","24","7","4.35","12","10");
+
+        $now = time();
+
+        $difference     = $now - $time;
+        $tense         = "ago";
+
+        for($j = 0; $difference >= $lengths[$j] && $j < count($lengths)-1; $j++) {
+            $difference /= $lengths[$j];
+        }
+
+        $difference = round($difference);
+
+        if($difference != 1) {
+            $periods[$j].= "s";
+        }
+
+        if($j == 0){
+            return "just now";
+        }else {
+            return "$difference $periods[$j] ago";
+        }
+    }
 }
