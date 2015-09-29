144c8fe7b5d88834d833881ce164c2c81db5e43c
SvetlinYotov <svetlin.yotov@gmail.com>
2015-09-29 21:52:03 +0300

    Bug fixes

--- app/controllers/BaseController.php
+++ app/controllers/BaseController.php
@@ -3,7 +3,18 @@
 namespace App\Controllers;
 
 
+use SSFrame\View;
+
 class BaseController
 {
 
+    /**
+     * @var View
+     */
+    public $view;
+
+    public function __construct()
+    {
+        $this->view = View::getInstance();
+    }
 }
