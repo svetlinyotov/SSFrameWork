4518d45e572864555dddcecf9e1f2b9b855ace57
SvetlinYotov <svetlin.yotov@gmail.com>
2015-09-29 21:57:09 +0300

    Register user

--- app/models/User.php
+++ app/models/User.php
@@ -4,16 +4,19 @@ namespace App\Models;
 
 
 use SSFrame\DB\SimpleDB;
+use SSFrame\Facades\Auth;
 use SSFrame\Facades\DB;
 use SSFrame\Hash;
 
 class User extends SimpleDB
 {
-    public static function create($email, $password, $names)
+    public function create($email, $password, $names)
     {
         $password = Hash::make($password);
 
-        DB::insert("INSERT INTO users (email, password, names) VALUES (?, ?, ?)", [$email, $password, $names]);
+        $last_id = $this->sql("INSERT INTO users (email, password, names) VALUES (?, ?, ?)", [$email, $password, $names])->getLastInsertId();
+
+        Auth::byId($last_id);
     }
 
 }
