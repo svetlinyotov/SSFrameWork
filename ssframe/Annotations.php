c16bb9bb1bc1308882b24e187b442a84f919eb32
SvetlinYotov <svetlin.yotov@gmail.com>
2015-10-03 03:02:22 +0300

    Bug fixes

--- ssframe/Annotations.php
+++ ssframe/Annotations.php
@@ -53,7 +53,7 @@ class Annotations
     {
         if(\SSFrame\Facades\Auth::user() == true) {
             $roles = array_map('trim', explode(",", $id));
-            $current_role = \SSFrame\Facades\Auth::user()->role;
+            $current_role = \SSFrame\Facades\Auth::user()->role-1;
 
             if (array_search($current_role, $roles) === false) {
                 throw new \Exception("Current user's role not authorized", 402);
